fd96270036c79654f91af05ac86d246f09e47f2f
Weinstein312 <jacob.weinstein@runi.ac.il>
2023-12-20 19:41:21 +0200

    progress 1

--- Coins.java
+++ Coins.java
@@ -4,6 +4,12 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here HELLO I AM A CHANGE LOOK AT ME GO
+		// Put your code here 
+		int coins = Integer.parseInt(args[0]);
+
+		int cents = coins % 25;
+		int quarters = coins / 25;
+
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents.");
 	}
 }
