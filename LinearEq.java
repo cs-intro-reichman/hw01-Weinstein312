cb38238ad476751b4d9c02daf432c67e62e24e6e
Weinstein312 <jacob.weinstein@runi.ac.il>
2023-12-20 21:17:05 +0200

    progress 2

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,15 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	public static void main(String[] args) {
+		// Put your code here
+		float a = Float.parseFloat(args[0]);
+		float b = Float.parseFloat(args[1]);
+		float c = Float.parseFloat(args[2]);
+
+		float solution = (c - b)/a;
+
+		System.out.println(a + "x + " + b + " = " + c);
+		System.out.println("x = " + solution);
+	}
 }
